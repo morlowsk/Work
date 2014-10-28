d59d7a6ef0fd5f0bb03806e6b94928a701681571
morlowsk <michael_orlo@ameritech.net>
2014-10-28 18:37:59 -0500

    Delete fourier.py

--- Italian_Leather_Store/Project/fourier.py
+++
@@ -1,8 +0,0 @@
-import cx_Oracle
-con = cx_Oracle.connect('morlowsk', 'weaselthorptlahuac1', cx_Oracle.makedsn('fourier.cs.iit.edu', 1521, 'orcl'))
-cursor = con.cursor()
-cursor.execute("select * from gkumar7.categories") #keep this as gkumar7
-
-print [i[0] for i in cursor.description]
-for col in cursor:
-  print col
